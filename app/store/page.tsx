cea4035c7a63a617ef646614cc9bb81c36734b17
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-12 23:58:08 +0100

    flex and carousel added

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -4,6 +4,9 @@ import { Product } from '@/types/Product';
 import { useEffect, useState } from 'react';
 import StoreCard from '../componenets/StoreCard';
 import StoreNav from '../componenets/StoreNav';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
 
@@ -21,22 +24,26 @@ export default function Home() {
     <main className='mt-16 md:mt-[80px] z-10 bg-black'>
       <StoreNav />
       <section className='content mb-4 w-full text-xl md:text-2xl py-12'>
-        <h2 className='text-[#44d62c] text-2xl font-bold'>
-          THE LATEST AND GREATEST GAMING GEAR
-        </h2>
-        <p className='text-[#7F7D7F] text-lg '>
-          Razer mice, keyboards, headsets, laptops & more
-        </p>
+        <div className='w-5/6 mx-auto'>
+          <h2 className='text-[#44d62c] text-2xl md:text-4xl font-bold'>
+            THE LATEST AND GREATEST GAMING GEAR
+          </h2>
+          <p className='text-[#7F7D7F] text-lg md:text-xl'>
+            Razer mice, keyboards, headsets, laptops & more
+          </p>
+        </div>
       </section>
       <section className='store'>
         <div className='w-5/6 mx-auto flex flex-wrap justify-center items-center gap-8 '>
-          {products &&
-            products.map((product: Product) => (
-              <StoreCard
-                key={product._id}
-                product={product}
-              />
-            ))}
+          <Carousel>
+            {products &&
+              products.map((product: Product) => (
+                <StoreCard
+                  key={product._id}
+                  product={product}
+                />
+              ))}
+          </Carousel>
         </div>
       </section>
     </main>
