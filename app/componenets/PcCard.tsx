b631074936dd0446e44e35003c89841d15005645
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-17 21:43:41 +0100

    single prodcut changed

--- app/componenets/PcCard.tsx
+++ app/componenets/PcCard.tsx
@@ -28,17 +28,17 @@ const PcCard = ({ product }: PcCardProps) => {
           className='rounded-t-md transition-transform duration-300 hover:scale-105'
         />
         <div className='p-5'>
-          <h5 className='mb-2 text-2xl font-bold tracking-tight text-white'>
+          <h5 className='mb-2 text-lg font-bold tracking-tight text-white'>
             {product.name.toUpperCase()}
           </h5>
 
-          <p className='mb-3 font-normal text-[#888]'>
+          <p className='mb-3 font-normal text-[#888] text-sm'>
             {product.excerpt &&
               (product.excerpt.length > 25
                 ? product.excerpt.slice(0, 45) + '...'
                 : product.excerpt)}
           </p>
-          <p className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#44d62c] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+          <p className='inline-flex items-center px-3 py-2 text-xs font-medium text-center text-[#44d62c] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
             Check it out
             <svg
               className='w-3.5 h-3.5 ml-2'
