bf94c1627c92fd3ce14628b3d24cf799cb878901
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-21 22:17:40 +0100

    slg page

--- app/pc/[slug]/page.tsx
+++ app/pc/[slug]/page.tsx
@@ -35,33 +35,38 @@ const Page = ({ params }: Props) => {
 
   return (
     <div className='mt-16  z-10 bg-[#252525]'>
-      <p>{product?.name}</p>
-      <p>{product?.slug}</p>
-      <p>{product?.price}</p>
-      <section className='carousel'>
-        {product?.images && (
-          <Image
-            src={urlFor(product.images[0]).width(650).height(650).url()}
-            alt={product.name}
-            height='650'
-            width='650'
-            className='w-full h-[400px] object-contain'
-            loading='lazy'
-          />
-        )}
-        <div className='absolute left-4 md:left-10 top-10'>
-          <div className='my-4 flex flex-col gap-y-4 gap-x-4 items-center'>
-            {product?.images &&
-              product.images.map((img: string) => (
-                <Image
-                  src={urlFor(img).width(650).height(650).url()}
-                  alt={product.name}
-                  height='650'
-                  width='650'
-                  className='  cursor-pointer  w-20 h-24 p-2 border border-gray-700 bg-black transition-all ease-in-out duration-300'
-                  loading='lazy'
-                />
-              ))}
+      <p className='text-green-500 capitalize text-lg font-bold mb-3'>
+        {product?.name}
+      </p>
+
+      <section className='flex flex-col md:flex-row gap-y-28 w-full gap-x-10"'>
+        <div className='w-full relative'>
+          <div>
+            {product?.images && (
+              <Image
+                src={urlFor(product.images[0]).width(650).height(650).url()}
+                alt={product.name}
+                height='650'
+                width='650'
+                className='w-full h-[400px] object-contain'
+                loading='lazy'
+              />
+            )}
+          </div>
+          <div className='absolute left-4 md:left-10 top-10'>
+            <div className='my-4 flex flex-col gap-y-4 gap-x-4 items-center'>
+              {product?.images &&
+                product.images.map((img: string) => (
+                  <Image
+                    src={urlFor(img).width(650).height(650).url()}
+                    alt={product.name}
+                    height='650'
+                    width='650'
+                    className='  cursor-pointer  w-20 h-24 p-2 border border-gray-700 bg-black transition-all ease-in-out duration-300'
+                    loading='lazy'
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </section>
