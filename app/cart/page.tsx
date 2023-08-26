6d5e88092b0d67830758602a9f41484032216c37
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-26 10:54:10 +0100

    page conditional rednering

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,27 @@ import { useCartStore } from '../componenets/store/useCartStore';
 
 const page = () => {
   const { cartItems } = useCartStore();
-  return <div>page</div>;
+  return (
+    <div>
+        {cartItems > 0
+                  ? ( <div className='container mx-auto my-20'>
+                  <div className='flex flex-col items-center min-h-[40vh] justify-center p-2 gap-y-4'>
+                    <span className='capitalize text-xl md:text-2xl'>
+                      your cart is empty eje mi..
+                    </span>
+                    <a
+                      className='uppercase hover:text-red-500 text-sm underline'
+                      href='/'
+                    >
+                      Go back home and buy something
+                    </a>
+                  </div>
+                </div>)
+                  : ' Cart Working'}
+                  
+     
+    </div>
+  );
 };
 
 export default page;
