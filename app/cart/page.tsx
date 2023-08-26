5891af1e2a802c5b637abdd6c07924bf63415311
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-26 15:59:39 +0100

    cat page updated

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -5,7 +5,14 @@ import React from 'react';
 import { useCartStore } from '../componenets/store/useCartStore';
 
 const Cart = () => {
-  const { cartItems } = useCartStore();
+  const { cartItems, cart } = useCartStore();
+  let total = 0;
+  if (cart) {
+    total = cart.reduce(
+      (acc, product) => acc + product.price * (product.quantity as number),
+      0
+    );
+  }
   return (
     <div className='mt-16'>
       {cartItems === 0 ? (
@@ -23,7 +30,11 @@ const Cart = () => {
           </div>
         </div>
       ) : (
-        ' Cart Working'
+        <div>
+          <div className='bg-stone-900 md:px-10 w-full p-4 flex justify-between items-center capitalize'>
+            <h4>your cart total is US${total}</h4>
+          </div>
+        </div>
       )}
     </div>
   );
