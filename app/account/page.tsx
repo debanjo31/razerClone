d6b5d666da2b69f2039f562a999899493d301ce5
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-28 23:43:43 +0100

    added acount pag

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -6,7 +6,7 @@ export default async function ServerPage() {
   const session = await getServerSession(options);
 
   if (!session) {
-    redirect('/api/auth/signin?callbackUrl=/server');
+    redirect('/account/signin?callbackUrl=/server');
   }
 
   return (
---
+++ app/account/signin/page.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <main className='section h-max md:h-screen'>
+      <div className='absolute -z-10'>
+        {/* <img alt="" srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcar.e93dc940.jpg&amp;w=2048&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcar.e93dc940.jpg&amp;w=3840&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcar.e93dc940.jpg&amp;w=3840&amp;q=75"
+          width="2000" height="2000" decoding="async" data-nimg="1" className="" loading="lazy" style="color: transparent;">
+             */}
+      </div>
+      <div className='flex items-center justify-center py-20'>
+        <div className='border md:w-[35%] border-black bg-black text-white p-6'>
+          <h1 className='uppercase text-2xl'>Razer Login</h1>
+          <div className='my-5 flex flex-col gap-y-4'>
+            <input
+              type='text'
+              placeholder='enter email'
+              className='w-full placeholder:uppercase bg-transparent border-gray-700'
+            />
+            <input
+              type='password'
+              placeholder='enter password'
+              className='w-full placeholder:uppercase bg-transparent border-gray-700'
+            />
+            <div className='my-5'>
+              <button className='w-full uppercase font-bold p-2 bg-green-600'>
+                login
+              </button>
+            </div>
+            <div>
+              <p className='text-sm text-center text-gray-400'>
+                <span className='font-bold text-red-700'>NOTE</span>, only sign
+                in with google auth, others dont work yet
+              </p>
+            </div>
+            <div className='py-5 border-t'>
+              <button className='w-full bg-gray-100 uppercase p-2 text-black font-bold hover:bg-gray-300'>
+                Sign in with google
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
