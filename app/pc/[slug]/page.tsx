02dfb55926682ebb0bf77bf87630fedc22244471
Adebafnjo Israel <debanjo31@gmail.com>
2023-08-31 01:31:19 +0100

    styling added

--- app/pc/[slug]/page.tsx
+++ app/pc/[slug]/page.tsx
@@ -36,10 +36,10 @@ const Page = ({ params }: Props) => {
 
   return (
     <div className='mt-16  z-10 bg-[#252525]'>
-      <p className='text-[#44d62c] capitalize text-lg font-bold mb-4 md:hidden'>
+      <p className='text-[#44d62c] capitalize text-lg font-bold mb-4 md:hidden w-5/6 mx-auto'>
         {product?.name}
       </p>
-      <section className='mb-12 md:flex'>
+      <section className='mb-12 md:flex md:w-5/6 mx-auto'>
         <div className='w-full relative md:w-4/6'>
           <div>
             {product?.images && product.images[slideNumber] && (
@@ -80,11 +80,11 @@ const Page = ({ params }: Props) => {
             </div>
           </div>
         </div>
-        <div>
+        <div className='flex flex-col justify-between gap-4 mt-8'>
           <p className='text-[#44d62c] capitalize text-lg font-bold mb-4 hidden md:block'>
             {product?.name}
           </p>
-          <p className='text-sm text-gray-300'>{product?.description}</p>
+          <p className='text-sm text-gray-300'>{product?.excerpt}</p>
           <p className='text-lg text-white'>{product?.price}</p>
         </div>
       </section>
