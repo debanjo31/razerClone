45fc6d502be191da50f424ef7286d2acf808675f
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-02 23:33:04 +0100

    signout func

--- app/account/signout.tsx
+++ app/account/signout.tsx
@@ -2,7 +2,7 @@
 import { signOut } from 'next-auth/react';
 
 export default () => (
-  <div className='flex justify-between items-center'>
+  <div className='flex justify-center items-center'>
     <button
       onClick={() => signOut()}
       className='block mt-8 bg-[hsl(112,67%,51%)] text-white p-2 rounded-md'
