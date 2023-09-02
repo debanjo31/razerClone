1be3aecd6d7a20e2a6b9e073956c866d88b60a87
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-02 11:44:18 +0100

    cart code updated

--- app/account/signout.tsx
+++ app/account/signout.tsx
@@ -1,5 +1,3 @@
 import { signOut } from 'next-auth/react';
 
-export default () => (
-  <button onClick={() => signOut({ callbackUrl: '/store' })}>Sign out</button>
-);
+export default () => <button onClick={() => signOut()}>Sign out</button>;
