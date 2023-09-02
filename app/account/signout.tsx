8acb768437eaba50717c9583d96de36984efac38
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-02 01:20:21 +0100

    callback url added

--- app/account/signout.tsx
+++ app/account/signout.tsx
@@ -1,11 +1,5 @@
 import { signOut } from 'next-auth/react';
 
 export default () => (
-  <button
-    onClick={() =>
-      signOut({ callbackUrl: 'https://razersclone.vercel.app/store' })
-    }
-  >
-    Sign out
-  </button>
+  <button onClick={() => signOut({ callbackUrl: '/store' })}>Sign out</button>
 );
