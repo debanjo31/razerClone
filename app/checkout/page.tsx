17553df84dda3b761882c412703e552088224806
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-03 22:53:12 +0100

    added paystsack

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,7 @@
 import { options } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
+import Paystack from './payment';
 
 export default async function ServerPage() {
   const session = await getServerSession(options);
@@ -12,6 +13,7 @@ export default async function ServerPage() {
   return (
     <section className='flex flex-col gap-6 mt-16'>
       <p>Checkouts</p>
+      <Paystack />
     </section>
   );
 }
