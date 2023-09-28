d03a5e2f19ce7cea20ea21a3cda5e2da6354454b
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-29 00:56:56 +0100

    aded changes to slug page

--- app/componenets/Features.tsx
+++ app/componenets/Features.tsx
@@ -5,14 +5,13 @@ import config from '../../sanity/config/client-config';
 interface Feature {
   title: string;
   description: string;
+  subtitle?: string;
   pic?: {
     asset?: {
       _ref?: string | undefined;
     };
   };
 }
-
-
 
 interface FeaturesProps {
   feature: Feature;
@@ -26,18 +25,24 @@ const Features = ({ feature }: FeaturesProps) => {
   }
 
   return (
-    <div className='text-white'>
-      <h1>{feature.title}</h1>
-      <p>{feature.description}</p>
-      {feature.pic?.asset._ref != undefined && (
-        <Image
-          src={urlFor(feature.pic.asset._ref).width(450).height(450).url()}
-          alt={feature.title}
-          height='450'
-          width='450'
-          className='w-36 h-36 block my-4 mx-auto'
-        />
+    <div className='text-white text-center'>
+      <h1 className='text-[#44d62c] '>{feature.title}</h1>
+      {feature.subtitle != undefined && (
+        <h3 className=''>{feature.subtitle}</h3>
       )}
+
+      <p className='text-gray-500'>{feature.description}</p>
+      <div className='w-full h-[40vh]'>
+        {feature.pic?.asset._ref != undefined && (
+          <Image
+            src={urlFor(feature.pic.asset._ref).width(450).height(450).url()}
+            alt={feature.title}
+            fill
+            style={{ objectFit: 'cover' }}
+            className='w-36 h-36 block my-4 mx-auto'
+          />
+        )}
+      </div>
     </div>
   );
 };
