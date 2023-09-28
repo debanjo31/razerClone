a6477ea8888fafb8ac6cbb475f9baf7378ef79ac
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-28 23:31:27 +0100

    removed payment

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 import { options } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
-import Paystack from './payment';
+
 import Form from './checkout';
 import Head from 'next/head';
 import type { Metadata } from 'next';
@@ -31,12 +31,6 @@ export default async function ServerPage() {
       </Head>
       <section className='bg-black text-white md:min-h-screen py-20'>
         <div className='max-w-4xl mx-auto bg-ct-dark-100 rounded-md mb-2 flex justify-center items-center'>
-          {/* {typeof user?.name === 'string' && typeof user?.email === 'string' ? (
-          <Paystack
-            name={user.name}
-            email={user.email}
-          />
-        ) : null} */}
           {typeof user?.name === 'string' && typeof user?.email === 'string' ? (
             <Form
               name={user.name}
