a1647237a292ea1cc6504f1fb379d9f253b80fae
Adebafnjo Israel <debanjo31@gmail.com>
2023-09-30 22:36:11 +0100

    added messages

--- app/componenets/Features.tsx
+++ app/componenets/Features.tsx
@@ -26,20 +26,20 @@ const Features = ({ feature }: FeaturesProps) => {
 
   return (
     <div className='text-white text-center'>
-      <h1 className='text-[#44d62c] '>{feature.title}</h1>
-      {feature.subtitle != undefined && (
+      <h1 className='text-[#44d62c] text-lg mb-2'>{feature.title}</h1>
+      {feature.subtitle !== undefined && (
         <h3 className=''>{feature.subtitle}</h3>
       )}
 
       <p className='text-gray-500'>{feature.description}</p>
-      <div className='w-full h-[40vh]'>
-        {feature.pic?.asset._ref != undefined && (
+      <div className='w-full h-[40vh] relative'>
+        {feature.pic?.asset._ref !== undefined && (
           <Image
             src={urlFor(feature.pic.asset._ref).width(450).height(450).url()}
             alt={feature.title}
-            height='450'
-            width='450'
-            className='w-36 h-36 block my-4 mx-auto'
+            fill
+            style={{ objectFit: 'cover' }}
+            className='block my-4 mx-auto'
           />
         )}
       </div>
